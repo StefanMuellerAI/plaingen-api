8445602a60215b22c2273e61a8c4008595637208
DaVinciKollektiv <118380549+DaVinciKollektiv@users.noreply.github.com>
2024-11-20 21:55:59 +0100

    API Performance Optimierung

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 import uvicorn
-import multiprocessing
 
 if __name__ == "__main__":
-    workers = multiprocessing.cpu_count() * 2 + 1
     uvicorn.run(
         "api:app",
         host="0.0.0.0",
         port=8000,
-        workers=workers,
         loop="uvloop",
-        http="httptools"
+        http="httptools",
+        workers=4
     ) 
